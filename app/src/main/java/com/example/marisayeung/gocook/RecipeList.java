9957d2331ed8f16a438678459435a10b6189fbed
marisayeung <marisayeung1@gmail.com>
2016-02-18 02:30:27 -0800

    Detail Activity. Rough recipe detail layout.

--- app/src/main/java/com/example/marisayeung/gocook/RecipeList.java
+++ app/src/main/java/com/example/marisayeung/gocook/RecipeList.java
@@ -1,7 +1,6 @@
 package com.example.marisayeung.gocook;
 
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
